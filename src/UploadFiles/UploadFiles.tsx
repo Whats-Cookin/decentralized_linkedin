bf4308ba7bb03863c67e9c2e907387da69a6789d
AmrNabel <121950000+AmrNabel@users.noreply.github.com>
2023-07-15 14:55:17 +0300

    Upload Files

---
+++ src/UploadFiles/UploadFiles.tsx
@@ -0,0 +1,191 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Divider,
+  TextField,
+  Typography,
+} from "@mui/material";
+import ControlPointIcon from "@mui/icons-material/ControlPoint";
+import React from "react";
+
+const UploadFiles = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#FFFFFF",
+        minHeight: "100vh",
+        width: "65%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Box
+        sx={{
+          width: "787px",
+          height: "205px",
+          m: "175px auto 55px 64px",
+          padding: "15px 16px 10px 41px",
+          border: "1px solid #0A0A0A33",
+          borderRadius: "5px",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "24px",
+              fontWeight: "500",
+              lineHeight: "29.05px",
+              fontFamily: "Inter",
+            }}
+          >
+            Achievements
+          </Typography>
+          <Button
+            onClick={handleClickOpen}
+            sx={{
+              fontSize: "22px",
+              fontWeight: "500",
+              lineHeight: "26.63px",
+              color: "#432335",
+            }}
+          >
+            +IMPORT
+          </Button>
+          <Dialog
+            open={open}
+            keepMounted
+            onClose={handleClose}
+            aria-describedby="alert-dialog-slide-description"
+          >
+            <DialogTitle>{"Import File"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-slide-description">
+                Improt your Achievements file from your PC
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Close</Button>
+              <Button onClick={handleClose}>Import</Button>
+            </DialogActions>
+          </Dialog>
+        </Box>
+        <Divider style={{ height: 1, width: "100%", color: "#00000033" }} />{" "}
+        <Box
+          sx={{
+            width: "520px",
+            height: "48px",
+            mt: "45px",
+            border: "1px solid #0A0A0A33",
+            borderRadius: "5px",
+            alignItems: "left",
+            display: "flex",
+            ml: "10px",
+          }}
+        >
+          {" "}
+          <Button>
+            <ControlPointIcon sx={{ color: "#757575", fontWeight: "500" }} />
+          </Button>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              fontWeight: "500",
+              lineHeight: "19.36pxpx",
+              width: "247px",
+              color: "#707070",
+              m: "auto 0",
+            }}
+          >
+            Add your Achievements
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          width: "787px",
+          height: "221px",
+          m: "0 auto 55px 64px",
+          border: "1px solid #0A0A0A33",
+          borderRadius: "5px",
+          padding: "15px 16px 15px 41px",
+        }}
+      >
+        <Typography
+          sx={{ fontSize: "24px", fontWeight: "500", lineHeight: "29.05px" }}
+        >
+          Recommendations
+        </Typography>
+        <Divider
+          style={{
+            height: 1,
+            width: "100%",
+            color: "#00000033",
+            marginTop: "10px",
+            marginBottom: "25px",
+          }}
+        />
+        <Typography
+          sx={{
+            fontSize: "16px",
+            fontWeight: "500",
+            lineHeight: "19.36px",
+            color: "#707070",
+            ml: "7px",
+          }}
+        >
+          Request a recommendation
+        </Typography>
+        <Box sx={{ display: "flex", ml: "7px" }}>
+          <TextField
+            label="Email"
+            sx={{
+              width: "386px",
+              padding: "14px 126px 13 px 13px",
+              mt: "10px",
+              height: "48px",
+            }}
+          />
+          <Button
+            sx={{
+              color: "#ffffff",
+              fontSize: "20px",
+              fontWeight: "500",
+              backgroundColor: "#432335",
+              width: "122px",
+              height: "55px",
+              border: "1px solid #0A0A0A33",
+              borderRadius: "5px",
+              m: " auto 14px",
+              mt: "10px",
+            }}
+          >
+            Send
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default UploadFiles;
--- src/profile/index.tsx
+++ src/profile/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import IProfileProps from './types'
+import IProfileProps from "./types";
+import UploadFiles from "../UploadFiles/UploadFiles";
+import { Box } from "@mui/material";
 
-const Profile = ({toggleSnackbar,setSnackbarMessage}: IProfileProps )=> {
+const Profile = ({ toggleSnackbar, setSnackbarMessage }: IProfileProps) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box sx={{ display: "flex",justifyContent: "flex-end",width:'100%',backgroundColor:'#849EA6' }}>
+      <UploadFiles />
+    </Box>
+  );
+};
 
-export default Profile
+export default Profile;
