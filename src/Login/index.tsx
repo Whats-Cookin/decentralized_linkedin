a1a985d93935bf4aa55b02a851d5f02808e7eac7
Zakia Mangal <zakiaimran513@gmail.com>
2023-07-26 13:46:37 +0430

    colorlogo

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,98 +1,117 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import Button from '@mui/material/Button'
-import ILoginProps from './types';
-import { Box, TextField, Typography } from '@mui/material'
-import styles from './styles';
+import React from "react";
+import { useForm } from "react-hook-form";
+import Button from "@mui/material/Button";
+import ILoginProps from "./types";
+import { Box, TextField, Typography } from "@mui/material";
+import styles from "./styles";
 
-
-const Login = ({toggleSnackbar,setSnackbarMessage,setLoading}: ILoginProps) => {
-  const{
+const Login = ({
+  toggleSnackbar,
+  setSnackbarMessage,
+  setLoading,
+}: ILoginProps) => {
+  const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
- 
-    
-  const onSubmit = handleSubmit(async({email, password})=>{
-    console.log("you pressed submit, congratulations")
-    try{
-      if(!email || !password){
-        toggleSnackbar(true)
-        setSnackbarMessage("Both email and password")
-      }else{
-        setLoading(true)
+  const onSubmit = handleSubmit(async ({ email, password }) => {
+    console.log("you pressed submit, congratulations");
+    try {
+      if (!email || !password) {
+        toggleSnackbar(true);
+        setSnackbarMessage("Both email and password");
+      } else {
+        setLoading(true);
       }
-    }catch (err:any){
-      setLoading(false)
-      toggleSnackbar(true)
-      setSnackbarMessage("user not found")
-      console.error("Error:",err?.message)
+    } catch (err: any) {
+      setLoading(false);
+      toggleSnackbar(true);
+      setSnackbarMessage("user not found");
+      console.error("Error:", err?.message);
     }
-  })
+  });
 
   return (
-    <form onSubmit={onSubmit} style={{ zIndex: 2, width: '100%', maxWidth: '430px', margin: '0 auto' }}>
+    <form
+      onSubmit={onSubmit}
+      style={{
+        zIndex: 2,
+        width: "100%",
+        maxWidth: "430px",
+        margin: "0 auto",
+        marginRight: "17%",
+        justifyContent: "center",
+      }}
+    >
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
           rowGap: 2,
-          width: '100%',
-          padding: '2rem',
-          maxWidth: '430px',
+          width: "100%",
+          padding: "2rem",
+          maxWidth: "430px",
+          margin: "0 auto",
+
           marginTop: { xs: 15, md: 8 },
-          background: '#FFFFFF',
-          boxShadow: '0px 1px 20px rgba(0, 0, 0, 0.25)',
+          background: "#FFFFFF",
+          boxShadow: "0px 1px 20px rgba(0, 0, 0, 0.25)",
           zIndex: 20,
-          borderRadius: '10px'
-        }}>
+          borderRadius: "10px",
+        }}
+      >
         <Typography
-          variant='h5'
+          variant="h5"
           style={{
-            textAlign: 'center',
-            fontWeight: 'bold',
-            fontSize: '2.5rem'
+            textAlign: "center",
+            fontWeight: "bold",
+            fontSize: "2.5rem",
           }}
-          sx={{ color: 'primary.main' }}
+          sx={{ color: "primary.main" }}
         >
           Login
         </Typography>
         <TextField
-          {...register('email', {
-            required: 'Email is required',
+          {...register("email", {
+            required: "Email is required",
             pattern: {
               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: 'Invalid email address'
-            }
+              message: "Invalid email address",
+            },
           })}
           fullWidth
-          label='Email'
+          label="Email"
           sx={styles.inputField}
-          variant='filled'
-          type='email'
-          helperText={(errors.email?.message as string) || ''}
+          variant="filled"
+          type="email"
+          helperText={(errors.email?.message as string) || ""}
           error={!!errors.email}
         />
         <TextField
-          {...register('password',{
-            required:"Password is required",
+          {...register("password", {
+            required: "Password is required",
           })}
           fullWidth
-          label='Password'
+          label="Password"
           sx={styles.inputField}
-          variant='filled'
-          type='password'
-          helperText={(errors.password?.message as string) || ''}
+          variant="filled"
+          type="password"
+          helperText={(errors.password?.message as string) || ""}
           error={!!errors.password}
         />
       </Box>
-      <Button sx={{ width: '100%' }} type='submit' variant='contained' size='medium'>
+      <Button
+        sx={{ width: "100%" }}
+        type="submit"
+        variant="contained"
+        size="medium"
+      >
         Login
       </Button>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
